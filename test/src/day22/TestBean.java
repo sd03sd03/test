3e10394c84900346d108484f0a2bcc93db21aa33
it <it@DESKTOP-F2NNV44>
2018-11-26 12:01:16 +0900

    깃테스트

---
+++ test/src/day22/TestBean.java
@@ -0,0 +1,44 @@
+package day22;
+
+public class TestBean {
+	   private int num;
+	   private String name;
+	   private int birth;
+	   private int tel;
+	   private String addr;
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getBirth() {
+		return birth;
+	}
+	public void setBirth(int birth) {
+		this.birth = birth;
+	}
+	public int getTel() {
+		return tel;
+	}
+	public void setTel(int tel) {
+		this.tel = tel;
+	}
+	
+	
+	public String getAddr() {
+		return addr;
+	}
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
+	  
+	   
+
+}
---
+++ test/src/day22/TestDBA.java
@@ -0,0 +1,87 @@
+package day22;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+
+
+
+
+public class TestDBA {
+	
+	Connection con;
+	   PreparedStatement ps;
+	   String url, user, pwd ; 
+	   //디비셋팅
+	   public TestDBA() {      
+	         try {
+	               Class.forName("oracle.jdbc.driver.OracleDriver");
+	               url="jdbc:oracle:thin:@localhost:1521:xe";
+	               user = "scott";
+	               pwd = "TIGER";       
+	               con = DriverManager.getConnection(url, user, pwd);
+	               } catch (ClassNotFoundException | SQLException e) {            
+	                  e.printStackTrace();         
+	               }
+	         }
+	   
+	   public ArrayList<TestBean> bookView(){
+	         Connection con = null;
+	         Statement st = null;
+	         ResultSet rs =null;
+	         ArrayList<TestBean> arr = new ArrayList<>();      
+	         try {
+	         con = DriverManager.getConnection(url, user, pwd);
+	         String sql = "select * from nametest";
+	         st = con.createStatement();
+	         rs = st.executeQuery(sql);
+	         while(rs.next()) {
+	        	 TestBean b = new TestBean();
+	            b.setNum(rs.getInt("num"));
+	            b.setName(rs.getString("name"));
+	            b.setBirth(rs.getInt("birth"));
+	            b.setTel(rs.getInt("tel"));
+	            b.setAddr(rs.getString("addr"));
+	            arr.add(b);
+	         }      
+	         }catch (SQLException e) {
+	            e.printStackTrace();      
+	         }finally{
+	         try {
+	            if(rs!=null)rs.close();
+	            if(st!=null)st.close();
+	            if(con!=null)con.close();      
+	         
+	         }catch (Exception e) {
+	            e.printStackTrace();      
+	         }
+	         }
+	         return arr;   
+	   }
+	   public void TestUpdate(TestBean b) {
+		   Connection con;
+		   PreparedStatement ps;
+	       try {      
+	           con = DriverManager.getConnection(url, user, pwd);
+	           String sql ="UPDATE nametest SET name=?,Birth=?,Tel=?,Addr=? WHERE NUM=?";
+	           ps = con.prepareStatement(sql);
+	           ps.setString(1, b.getName());
+	           ps.setInt(2, b.getBirth());         
+	           ps.setInt(3, b.getTel());      
+	           ps.setString(4, b.getAddr());   
+	           ps.setInt(5, b.getNum());
+	           ps.executeUpdate();         
+	           } catch (SQLException e) {            
+	              e.printStackTrace();         
+	           }         
+		   
+		   
+	   }
+	   
+
+}
